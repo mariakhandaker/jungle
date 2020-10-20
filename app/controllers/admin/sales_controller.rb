5ecd6c0b08cfbf23880aadff810319c82bf43fe1
Maria Khandaker <mariakhandaker@gmail.com>
2020-10-20 19:11:46 -0400

    more sale features

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -3,4 +3,5 @@ class Admin::SalesController < ApplicationController
   def index
     @sales = Sale.all
   end
+  
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,19 @@
 class Sale < ActiveRecord::Base
+  #AR scope
+  def self.active?
+    where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current.an)
+  end
+  
+  def finished?
+    ends_on < Date.current
+  end
+  
+  def upcoming?
+    starts_on > Date.current
+  end
+  
+  def active? 
+    !upcoming? && !finished?
+  end
+  
 end
